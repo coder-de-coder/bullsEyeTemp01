d0e1f6cb657a7cb9ff80edb4ddf6fec80a954673
Mayank <shanatic.mayank@gmail.com>
2024-06-26 15:06:12 +0530

    Innitial Commit (Boiler-Plate Turborepo setup with Postgresql db)

---
+++ apps/web/app/api/dbtest/route.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@repo/db";
+import { NextResponse } from "next/server";
+
+const client = new PrismaClient();
+
+export const GET = async () => {
+  try {
+    const userData = await client.user.findMany({});
+    const userNames = userData.map(user => user.name);
+
+    return NextResponse.json({
+      names: userNames,
+    });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({
+      error: 'An error occurred while fetching user data.',
+      e: e, // Return the error details in the response for debugging
+    });
+  } finally {
+    await client.$disconnect();
+  }
+};
